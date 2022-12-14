7065d987b78374cc149e0b973bbb362e1f0b9761
d0nd3r3k <donderekh@gmail.com>
2022-12-14 12:44:13 -0500

    refactor

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-mkdir /tmp/payload_sockets
-nc -vklU /tmp/payload_sockets/pl_sock &
-PID=$!
-sleep 1
-kill $PID
-
 touch /output.log
 python3 /home/payload/workspace/include/socket_coms_local.py &>> /output.log &
 
